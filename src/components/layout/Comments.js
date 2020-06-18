34fbf2e2f393fe52e39431bcb0163194008e5e68
LeoLanese <laneseleo@gmail.com>
2020-06-18 23:56:26 +0100

    cleanning code ++

--- src/components/layout/Comments.js
+++ src/components/layout/Comments.js
@@ -2,21 +2,23 @@ import React from 'react';
 
 export const Comment = function(props) {
   return (
-    <div className="Comment">
+    <div className="comment">
       <div className="container p-3 my-3 border wrapper-box">
-        <span>
-          <img
-            className="Avatar"
-            src={props.author.avatarUrl}
-            alt={props.author.name}
-          />
-          <h1 className="UserInfo-name">{props.author.name}</h1>
-        </span>
-        <p>{props.text}</p>
-        <p>{props.extraText}</p>
+        <div class="card text-center">
+          <div class="card-header">
+            <img
+                className="avatar"
+                src={props.author.avatarUrl}
+                alt={props.author.name}
+              />
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">{props.author.name}</h5>
+            <p class="card-text">{props.text}{props.extraText}</p>
+            <a href="mailto:developer@leolanese.com" class="btn btn-primary">Contact me!</a>
+          </div>
       </div>
-      <div className="UserInfo"></div>
-      <div className="Comment-text"></div>
+      </div>
     </div>
   );
 }
--- src/components/pages/SocialMedia.js
+++ src/components/pages/SocialMedia.js
@@ -4,31 +4,32 @@ export const SocialMedia = function(props) {
   return (
     <div>
       <div className="container p-3 my-3 border wrapper-box">
+        <div class="card">
+          <div class="card-header">
+            <h4>Thanks!</h4>
+          </div>
+          <div class="card-body">
+            <blockquote class="blockquote mb-0">
+              <p>'Leo Lanese',
+                  'Building Inspiring Responsive Reactive Solutions',
+                  'London, UK'</p>
+            </blockquote>
 
-        <h4> Thanks </h4>
-        <h5>'Leo Lanese',
-          'Building Inspiring Responsive Reactive Solutions',
-          'London, UK'
-        </h5>
-        <h5>Twitter:
-        <a href="http://twitter.com/LeoLaneseltd" target="_blank" rel="noreferrer noopener">twitter.com/LeoLaneseltd</a>
-        </h5>
-        <h5>Portfolio
-        <a href="http://www.leolanese.com" target="_blank" rel="noreferrer noopener">http://www.leolanese.com</a>
-        </h5>
-        <h5>DEV.to:
-        <a href="http://www.dev.to/leolanese" target="_blank" rel="noreferrer noopener">dev.to/leolanese</a>
-        </h5>
-        <h5>Blog:
-        <a href="http://www.leolanese.com/blog" target="_blank" rel="noreferrer noopener">leolanese.com/blog</a>
-        </h5>
-        <h5>Questions / Suggestion / Recommendation ?
-        <a href="mail:to">developer@leolanese.com</a>
-        </h5>
-
-      </div>
-
-
+              <h5>Twitter:
+              <a href="http://twitter.com/LeoLaneseltd" target="_blank" rel="noreferrer noopener"> twitter.com/LeoLaneseltd</a>
+              </h5>
+              <h5>Portfolio
+              <a href="http://www.leolanese.com" target="_blank" rel="noreferrer noopener"> http://www.leolanese.com</a>
+              </h5>
+              <h5>DEV.to:
+              <a href="http://www.dev.to/leolanese" target="_blank" rel="noreferrer noopener"> dev.to/leolanese</a>
+              </h5>
+              <h5>Blog:
+              <a href="http://www.leolanese.com/blog" target="_blank" rel="noreferrer noopener"> leolanese.com/blog</a>
+              </h5>
+              </div>
+          </div>
+        </div>
     </div>
   );
 };
