a06991304ba64bb3e078b4eccabb935897675357
leolanese <developer@leolanese.com>
2021-01-19 08:22:18 +0000

    code improvement

--- src/components/layout/title.js
+++ src/components/layout/title.js
@@ -4,7 +4,7 @@ export const Title = function(props) {
   return (
     <div className="Comment">
       <h1 className="display-4 mb-2">
-        <span className="text-info">Portfolio</span> Leo Lanese
+        <span className="text-info">Portfolio</span> Leo Lanese - SeaMar AM Ltd.
       </h1>
     </div>
     );
